4986a8410b20df0c0f175abf9299cae20d4c2c02
Ryan Palo <ryan@thepalos.com>
2016-08-16 09:36:05 -0700

    Finish plastic views

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
 from .models import Plastic 
 from .forms import PlasticForm
 
@@ -12,14 +12,32 @@ def index(request):
 def add(request):
 
     if request.method == "POST":
-        pass
+        form = PlasticForm(request.POST)
+        if form.is_valid():
+            newPlastic = form.save()
+
+            return HttpResponseRedirect('/inventory/')
     else:
         form = PlasticForm()
         return render(request, "inventory/form.html",
                         {"form": form})
 
 def detail(request, id):
+    plastic = get_object_or_404(Plastic, pk=id)
+    if request.method == "POST":
+        form = PlasticForm(request.POST, instance=plastic)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/inventory/')
+    else:
+        form = PlasticForm(instance=plastic)
+        return render(request, "inventory/form.html",
+                                    {"form": form,
+                                    "plastic": plastic})
+
     return HttpResponse("It works!")
 
 def delete(request, id):
-    return HttpResponse("It works!")
+    plastic = get_object_or_404(Plastic, pk=id)
+    plastic.delete()
+    return HttpResponseRedirect("/inventory/")
