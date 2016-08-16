af39e1d507ed9bf742bca8ce46a88ff0bb0a3d45
Ryan Palo <ryan@thepalos.com>
2016-08-16 10:28:39 -0700

    Add header/footer

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='plastic-home'),
     url(r'^add/$', views.add, name='add'),
     url(r'^detail/(?P<id>[0-9]+)/$', views.detail),
     url(r'^delete/(?P<id>[0-9]+)/$', views.delete, name='delete'),
