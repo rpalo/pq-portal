049d998f081e9b4df7758b711305dbb345f6330c
Ryan Palo <ryan@thepalos.com>
2016-08-16 10:07:24 -0700

    Add delete button to main page

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^add/$', views.add, name='add'),
     url(r'^detail/(?P<id>[0-9]+)/$', views.detail),
-    url(r'^delete/(?P<id>[0-9]+)/$', views.delete),
+    url(r'^delete/(?P<id>[0-9]+)/$', views.delete, name='delete'),
 ]
