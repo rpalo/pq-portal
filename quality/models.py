8a39746cb5d5f5a8bedab97909f2ad29be7b4cef
Ryan Palo <ryan@thepalos.com>
2017-02-10 16:09:44 -0800

    Layout initial quality classes.

--- quality/models.py
+++ quality/models.py
@@ -1,3 +1,37 @@
+"""Quality Models"""
+
+import datetime
 from django.db import models
 
-# Create your models here.
+
+class PrimaryQualityItem(models.Model):
+    """An abstract class to store the basic quality needs"""
+
+    STATUS_CHOICES = (("OPEN", "Open"),
+                      ("CLOSED", "Closed"))
+    number = models.CharField("document number", unique=True, max_length=30)
+    open = models.DateField("date opened", auto_now_add=True)
+    closed = models.DateField("date closed", blank=True, null=True)
+    customer = models.CharField(max_length=30, blank=True)
+    description = models.CharField(max_length=200, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+
+    def close(self):
+        """Marks the current document as closed"""
+
+        self.closed = datetime.date.today()
+        self.status = "CLOSED"
+        # Not sure if there needs to be a database save here or not
+        # No return value
+
+    def __str__(self):
+        return self.number
+
+    class Meta:
+        abstract = True
+
+class RMA(PrimaryQualityItem):
+    """Return Material Authorization"""
+    
+    def get_absolute_url(self):
+        return "/quality/rma/{}/".format(self.id)
